b6f2239d9dd5bc2335c3b1d1ae0e602e783c1c15
iLaviniu <42060058+iLaviniu@users.noreply.github.com>
2025-09-04 03:32:51 +0300

    Test package annotation added
    the main application class was specified

--- src/test/java/com/example/carins/CarInsuranceApplicationTests.java
+++ src/test/java/com/example/carins/CarInsuranceApplicationTests.java
@@ -1,15 +1,16 @@
 package com.example.carins;
 
-import com.example.carins.service.CarService;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
+import com.example.carins.service.CarService;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
+@SpringBootTest(classes = CarInsuranceApplication.class)
 class CarInsuranceApplicationTests {
 
     @Autowired
